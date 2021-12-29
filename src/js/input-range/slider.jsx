0bb4db54706f73ad6150a90c2f9b48f60794c264
eddyionescu <eddyionescu@gmail.com>
2021-12-28 20:59:02 -0500

    Change package json

--- src/js/input-range/slider.jsx
+++ src/js/input-range/slider.jsx
@@ -256,7 +256,7 @@ export default class Slider extends React.Component {
           aria-valuemax={this.props.maxValue}
           aria-valuemin={this.props.minValue}
           aria-valuenow={this.props.value}
-          // aria-valuetext={this.props.formatLabel || this.props.value}
+          aria-valuetext={this.props.formatLabel || this.props.value}
           className={this.props.classNames.slider}
           draggable="false"
           onKeyDown={this.handleKeyDown}
